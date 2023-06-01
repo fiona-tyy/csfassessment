bb81a30403fce03546c62b260e96f62a344e2bb1
fiona-tyy <zyyfiona@hotmail.com>
2023-06-01 09:09:15 +0800

    initial commit

---
+++ order-backend/src/main/java/ibf2022/batch3/assessment/csf/orderbackend/respositories/PendingOrdersRepository.java
@@ -0,0 +1,21 @@
+package ibf2022.batch3.assessment.csf.orderbackend.respositories;
+
+import org.springframework.stereotype.Repository;
+
+import ibf2022.batch3.assessment.csf.orderbackend.models.PizzaOrder;
+
+@Repository
+public class PendingOrdersRepository {
+
+	// TODO: Task 3
+	// WARNING: Do not change the method's signature.
+	public void add(PizzaOrder order) {
+	}
+
+	// TODO: Task 7
+	// WARNING: Do not change the method's signature.
+	public boolean delete(String orderId) {
+		return false;
+	}
+
+}
